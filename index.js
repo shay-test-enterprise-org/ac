86d17cf6bc5e9d3c988843bbccb8396b61f60bba
shay-legit <shay@legitsecurity.com>
2022-12-25 00:15:13 +0200

    save

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require("@actions/core");
-const fetch = await import("node-fetch");
-const tar = await import("tar");
-const execSync = await import("child_process");
+const fetch = require("node-fetch");
+const tar = require("tar");
+const execSync = require("child_process").execSync;
 
 const DOWNLOAD_URL = "https://github.com/Legit-Labs/legitify/releases/download";
 const VERSION = "v0.1.6";
