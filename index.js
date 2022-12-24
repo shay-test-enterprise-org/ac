66d6464cdc2001b6cf2aba180d92053df84b64aa
shay-legit <shay@legitsecurity.com>
2022-12-25 01:05:03 +0200

    save

--- index.js
+++ index.js
@@ -24,38 +24,24 @@ async function run() {
     const response = await fetch(
       `${DOWNLOAD_URL}/v${VERSION}/legitify_${VERSION}_linux_amd64.tar.gz`
     );
-
-    const fileBuffer = await response.buffer();
-
-    // Extract the tar stream from the gzip stream
-    const tarStream = zlib.gunzipSync(fileBuffer);
-
-    // Create a extractor to extract the tar stream
-    const extractor = tar.extract();
-
-    // Wait until file is fully extracted and ready to be executed
-    await new Promise((resolve, reject) => {
-      extractor.on("entry", (header, stream, next) => {
-        // Only extract the legitify executable
-        if (header.name === "legitify") {
-          // Write the executable to the file system
-          stream.pipe(fs.createWriteStream("./legitify"));
-          stream.on("end", () => {
-            // Make the executable executable
-            execSync("chmod +x ./ ./legitify");
-            // Resolve the promise
-            resolve();
-          });
-        } else {
-          // Skip the entry
-          stream.resume();
-        }
-        // Move to the next entry
-        stream.on("end", next);
-      });
-      // Pipe the tar stream to the extractor
-      tarStream.pipe(extractor);
+    const buffer = await response.buffer();
+    // Extract the file from the tarball
+    const extract = tar.extract();
+    extract.on("entry", (header, stream, next) => {
+      stream.on("end", next);
+      stream.resume();
     });
+    extract.on("finish", () => {
+      // Make the file executable
+      execSync("chmod +x ./legitify");
+      // Run the command
+      execSync(`./legitify ${command} --github-token ${token}`);
+    });
+    // Unzip the file
+    const gunzip = zlib.createGunzip();
+    gunzip.pipe(extract);
+    gunzip.write(buffer);
+    gunzip.end();
   } catch (error) {
     core.setFailed(error.message);
   }
