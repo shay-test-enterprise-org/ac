2de425541c4718053a5670ae83d60b6b322c8ee3
shay-legit <shay@legitsecurity.com>
2022-12-25 17:03:39 +0200

    save

--- index.js
+++ index.js
@@ -6,41 +6,6 @@ const https = require("https");
 const tar = require("tar-fs");
 const github = require("@actions/github");
 
-// isGitHubOrgOwner checks if the user is an owner of the GitHub organization
-// and returns true if they are, otherwise false
-async function isGitHubOrgOwner(token, org) {
-  const options = {
-    hostname: "api.github.com",
-    path: `/orgs/${org}/memberships/${github.context.actor}`,
-    method: "GET",
-    headers: {
-      Authorization: `token ${token}`,
-      "User-Agent": "Legitify",
-    },
-  };
-
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, (res) => {
-      let data = "";
-      res.on("data", (chunk) => {
-        data += chunk;
-      });
-      res.on("end", () => {
-        const response = JSON.parse(data);
-        if (response.state === "active") {
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      });
-    });
-    req.on("error", (error) => {
-      reject(error);
-    });
-    req.end();
-  });
-}
-
 async function run() {
   try {
     // Get the command input value
@@ -53,23 +18,8 @@ async function run() {
       return;
     }
 
-    const owner = core.getInput("GITHUB_REPOSITORY_OWNER");
-
-    let isOwner = isGitHubOrgOwner(token, owner);
-    if (!isOwner) {
-      core.setFailed("User is not an owner of the GitHub organization");
-      return;
-    }
-
-    // Get the operating system
-    const os = process.platform;
-
-    // set file URL based on operating system
-    // TODO: delete this logic
-    const fileUrl =
-      os == "darwin"
-        ? "https://github.com/Legit-Labs/legitify/releases/download/v0.1.6/legitify_0.1.6_darwin_arm64.tar.gz"
-        : "https://github.com/Legit-Labs/legitify/releases/download/v0.1.6/legitify_0.1.6_linux_amd64.tar.gz";
+    const legitifyVersion = core.getInput("legitify_version");
+    const fileUrl = `https://github.com/Legit-Labs/legitify/releases/download/v${legitifyVersion}/legitify_${legitifyVersion}_linux_amd64.tar.gz`;
 
     const filePath = "legitify.tar.gz";
 
@@ -103,7 +53,7 @@ async function run() {
               (error, stdout) => {
                 if (error) {
                   console.error(`Exec error: ${error}`);
-                  return;
+                  process.exit(1);
                 }
                 console.log(`${stdout}`);
               }
